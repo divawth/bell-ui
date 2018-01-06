b8ab10d78e0c3025ca0e25ccc19a50daa9641121
wangtianhua <13177771403@163.com>
2018-01-06 22:21:05 +0800

    update

---
+++ src/components/layout/BreadcrumbItem.js
@@ -0,0 +1,10 @@
+export default {
+    template: `
+        <span class="bell-bread-crumb-item">
+            {{$children}}
+            <span class="bell-bread-crumb-gap">
+                /
+            </span>
+        </span>
+    `
+}
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,7 @@
+export default {
+    template: `
+        <div class="bell-footer">
+            {{$children}}
+        </div>
+    `
+}
