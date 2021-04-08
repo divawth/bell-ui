1b264835673422743fee282a478b03d44ebc86d7
diva <752990270@qq.com>
2021-04-08 20:29:58 +0800

    bump 0.14.8

--- dist/bell-ui.d.ts
+++ dist/bell-ui.d.ts
@@ -1,7 +1,7 @@
 /**
  * 版本
  */
-export declare const version = "0.14.7";
+export declare const version = "0.14.8";
 /**
  * 安装插件
  */
--- types/index.d.ts
+++ types/index.d.ts
@@ -5,7 +5,7 @@ import './component/notification';
 /**
  * 版本
  */
-export declare const version = "0.14.7";
+export declare const version = "0.14.8";
 /**
  * 安装插件
  */
