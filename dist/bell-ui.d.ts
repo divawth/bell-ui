ba0a4cb0a65e2df3ca7a16f80590b1a71563223c
wth <752990270@qq.com>
2022-03-28 22:16:00 +0800

    bump 0.25.8

--- dist/bell-ui.d.ts
+++ dist/bell-ui.d.ts
@@ -1,7 +1,7 @@
 /**
  * 版本
  */
-export declare const version = "0.25.7";
+export declare const version = "0.25.8";
 /**
  * 安装插件
  */
--- types/index.d.ts
+++ types/index.d.ts
@@ -5,7 +5,7 @@ import './component/notification';
 /**
  * 版本
  */
-export declare const version = "0.25.7";
+export declare const version = "0.25.8";
 /**
  * 安装插件
  */
